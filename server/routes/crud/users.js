a4f97d3f275a9a1bf2036a406ac1c51864bc05e3
Tran Thanh Tung <39983442+thichraptancong@users.noreply.github.com>
2021-07-08 10:45:03 +0700

    test3

--- server/routes/crud/users.js
+++ server/routes/crud/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const { MESSAGES } = require('../../../constant/index');
 const UserModel = require('../../../models/user.model.js');
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { MESSAGES, KEY } = require('../../constant/index');
 import handlerVerifyUser from '../middleware/auth';
 
